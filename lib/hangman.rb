889ef626d61ae7ce370c576c57aac63af3d5be11
Elton <28992495+Lucrum@users.noreply.github.com>
2023-08-07 20:08:10 -0400

    File is closed upon choosing random word Chosen word is now validated to length criteria

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -5,14 +5,21 @@ require 'English'
 # main game class
 class Hangman
   def initialize
-    @word = random_word
+    @word = valid_word
+  end
+
+  def valid_word
+    word = random_word
+    word = random_word until word.length >= 5 && word.length <= 12
+    word
   end
 
   # this assumes the file is 10000 lines long!!
   def random_word
-    word_dict = File.open('google-10000-english-no-swears.txt')
-    rand(10_000).times { word_dict.gets }
-    $LAST_READ_LINE
+    File.open('google-10000-english-no-swears.txt', 'r') do |word_dict|
+      rand(1..10_000).times { word_dict.gets }
+      $LAST_READ_LINE.strip
+    end
   end
 end
 
